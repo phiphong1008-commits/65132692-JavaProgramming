fcc7300294b9b85676fb908544356852d2be7485
phiphong1008-commits <phiphong1008@gmail.com>
2026-01-23 14:10:23 +0700

    Hello Java
    Hello Java

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Xin chao Java");
+
+        System.out.println("1 + 2 + 3 =" +(1+2+3));
+    }
+}
