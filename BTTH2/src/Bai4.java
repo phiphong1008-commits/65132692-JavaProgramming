9d358b15461688ff75f15a10b63a7b71acd0552c
phiphong1008-commits <phiphong1008@gmail.com>
2026-02-01 17:01:00 +0700

    Update Bai4.java

--- BTTH2/src/Bai4.java
+++ BTTH2/src/Bai4.java
@@ -1,5 +1,22 @@
 public class Bai4 {
     public static void main(String[] args) {
-        
+        int soDien = 250;  // kWh
+        double tienDien = 0;
+
+        if (soDien <= 50) {
+            tienDien = soDien * 1678;
+        } else if (soDien <= 100) {
+            tienDien = 50 * 1678 + (soDien - 50) * 1734;
+        } else if (soDien <= 200) {
+            tienDien = 50 * 1678 + 50 * 1734 + (soDien - 100) * 2014;
+        } else if (soDien <= 300) {
+            tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + (soDien - 200) * 2536;
+        } else {
+            tienDien = 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + (soDien - 300) * 2834;
+        }
+
+        System.out.println("Số điện: " + soDien + " kWh");
+        System.out.println("Tiền điện: " + tienDien + " đồng");
+        System.out.println("Tiền điện (VND): " + String.format("PT,.0f", tienDien) + " đ");
     }
 }
